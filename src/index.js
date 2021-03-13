0e9ed5e2f1fc46af65f2e1ee72544e1848deecf5
Scaldy <45393189+Scaldy@users.noreply.github.com>
2021-03-13 11:45:30 -0300

    cambios 13_03_21

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 
-import './global.css';
+import './glouobal.css';
 import App from './components/App';
 
 const container = document.getElementById('app');
 
 ReactDOM.render(<App />, container);
+
+/* const jsx = <h1>Hello, Platzi Badges Marcelito!</h1>
+
+const container = document.getElementById('app');
+ReactDOM.render(jsx, container); */
+
+// const element = React.createElement(
+//     'a',
+//     {
+//         href: 'https://google.com'
+//     },
+//     'Ir a Google'
+// );
+
+// const container = document.getElementById('app');
+// ReactDOM.render(element, container);
